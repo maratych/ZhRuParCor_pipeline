dca8ad76ef29fea5cc4ee5adc600d616af23f027
NetBUG <oleg@skuuper.com>
2017-06-01 13:13:25 -0700

    Sample showing improvement over Mumu

--- marat_automation_sample.py
+++ marat_automation_sample.py
@@ -11,3 +11,5 @@ from compare.XML_compare import *
 file_golden = 'ZhRuParCor/data/mumu.xml'
 tmx = align('ZhRuParCor/original_texts/Mumu_r.txt', 'ZhRuParCor/original_texts/Mumu_zh.txt', ldc=True, lf=False, aligner='hunalign', df='ru-zh.dic')
 print(compare_data(file_golden, tmx))
+tmx = align('ZhRuParCor/original_texts/Mumu_r.txt', 'ZhRuParCor/original_texts/Mumu_zh.txt', ldc=True, lf=False, aligner='hunalign', df='ru-zhMARAT50k.dic')
+print(compare_data(file_golden, tmx))
